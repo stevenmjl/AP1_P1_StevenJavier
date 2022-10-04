07fbb1398396385a8a32038f7f8ec601e2d55b57
stevenmjl <stevenmjl.24@gmail.com>
2022-10-03 20:00:15 -0400

    Arreglando ConStr

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AP1_P1_StevenJavier.Models;
+
+namespace AP1_P1_StevenJavier.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+    }
+}
