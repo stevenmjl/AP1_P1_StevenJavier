f76119d4a8b19e0d03723cd57a7d65a8208c44e2
stevenmjl <stevenmjl.24@gmail.com>
2022-10-03 20:12:29 -0400

    Agregando migracion

---
+++ Migrations/20221004001100_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AP1_P1_StevenJavier.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aportes",
+                columns: table => new
+                {
+                    AportesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Persona = table.Column<string>(type: "TEXT", nullable: true),
+                    Observacion = table.Column<string>(type: "TEXT", nullable: false),
+                    Monto = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aportes", x => x.AportesId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aportes");
+        }
+    }
+}
